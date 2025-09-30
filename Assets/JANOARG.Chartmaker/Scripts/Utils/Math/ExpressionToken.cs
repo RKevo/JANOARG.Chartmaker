f3ee89860d7687ebe44ca971e255482c2faa4d12
RKevo <64652049+RKevo@users.noreply.github.com>
2025-09-30 14:48:17 +0700

    groundwork for parsing stuff

--- Assets/JANOARG.Chartmaker/Scripts/Utils/Math/ExpressionToken.cs
+++ Assets/JANOARG.Chartmaker/Scripts/Utils/Math/ExpressionToken.cs
@@ -43,4 +43,50 @@ namespace JANOARG.Chartmaker.Utils.Math
             return "')'";
         }
     }
+
+    // Expanded. In the hope to be usable as general storyboard scripting.
+    internal enum TokenKind
+    {
+        // Numeric
+        RtParen,
+        LParen,
+        Plus,
+        Minus,
+        Mul,
+        Div,
+        Modulo,
+        Exponent,
+        BitwiseAnd,
+        BitwiseOr,
+        BitwiseNegate,
+
+        // Logical
+        LogicalAnd,
+        LogicalOr,
+        LogicalNot,
+        Eq,
+        NEq,
+        GtEq,
+        LtEq,
+        Gt,
+        Lt,
+
+        // Constants
+        Identifier,
+        Number, // float?
+    }
+
+    internal struct SourceToken
+    {
+        public TokenKind Kind;
+        readonly Range tokenRange;
+        readonly int Begin => tokenRange.Begin;
+        readonly int End => tokenRange.End;
+
+        public struct Range
+        {
+            readonly public int Begin;
+            readonly public int End;
+        }
+    }
 }
