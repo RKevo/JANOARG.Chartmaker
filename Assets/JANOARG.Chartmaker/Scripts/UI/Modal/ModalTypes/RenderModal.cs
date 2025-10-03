2f2470e187fe0f3ff40767e18b4dff2e17536569
RKevo <64652049+RKevo@users.noreply.github.com>
2025-10-04 00:20:36 +0700

    spacing

--- Assets/JANOARG.Chartmaker/Scripts/UI/Modal/ModalTypes/RenderModal.cs
+++ Assets/JANOARG.Chartmaker/Scripts/UI/Modal/ModalTypes/RenderModal.cs
@@ -1170,7 +1170,7 @@ namespace JANOARG.Chartmaker.UI.Modal.ModalTypes
                 
                 sw.Stop();
                 UnityEngine.Debug.Log($"Config: {resolution.x}x{resolution.y}@{frameRate} {Prefs.VideoBitRate}kbit/sec");
-                    UnityEngine.Debug.Log($"Measurement ended (nat): {sw.Elapsed}");
+                UnityEngine.Debug.Log($"Measurement ended (nat): {sw.Elapsed}");
                 
                 
                 QualitySettings.antiAliasing = originalAntiAliasing;
