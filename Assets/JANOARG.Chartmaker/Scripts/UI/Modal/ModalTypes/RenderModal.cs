d3776fd5dbb0ddc8a0393b104180e87260d03da0
RKevo <64652049+RKevo@users.noreply.github.com>
2025-10-06 13:19:15 +0700

    sdsadwe

--- Assets/JANOARG.Chartmaker/Scripts/UI/Modal/ModalTypes/RenderModal.cs
+++ Assets/JANOARG.Chartmaker/Scripts/UI/Modal/ModalTypes/RenderModal.cs
@@ -1091,7 +1091,7 @@ namespace JANOARG.Chartmaker.UI.Modal.ModalTypes
 
                     if (pool.Rent(out var entry))
                     {
-                        var req = AsyncGPUReadback.RequestIntoNativeArray<byte>(ref entry._ref, tex, 0, TextureFormat.RGB24);
+                        var req = AsyncGPUReadback.RequestIntoNativeArray<byte>(ref entry._ref, rtex, 0, TextureFormat.RGB24);
                         while (!req.done)
                         {
                             await Task.Yield();
